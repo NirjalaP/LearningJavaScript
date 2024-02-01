b250f0db0b2adc6feac1bd048772119e7433b6da
NirjalaP <nirjala.prajapati11@gmail.com>
2024-02-01 22:11:05 +0545

    2

---
+++ 2.js
@@ -0,0 +1,8 @@
+// . Function that takes a string as input and returns its length.
+function strlen (str){
+    return str.length;
+}
+
+const input = "car";
+const len = strlen(input);
+console.log(len); // output = 3
