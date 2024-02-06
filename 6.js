b6a5154bd50ef5c54a70faca993b0f8dc3d69c41
NirjalaP <nirjala.prajapati11@gmail.com>
2024-02-06 23:35:27 +0545

    6

---
+++ 6.js
@@ -0,0 +1,14 @@
+// 6. Function that takes a string as input and checks if it contains the word "JavaScript". Return true if it does, false otherwise.
+function checkString(str){
+    return str.includes ("JavaScript");
+}
+
+const StringA =' This is JavaScript.';
+console.log (checkString(StringA));
+
+const StringB =' This is Nepal.';
+console.log (checkString(StringB));
+
+
+// console.log (checkString('This is JavaScript.'));
+// console.log (checkString('This is Python.'));
